cc968013fb389263e8829f27c77c49317bc17fa7
MaheshMurari <maheshmurari.data@gmail.com>
2025-04-10 17:22:14 +0530

    Add files via upload

---
+++ loops_prblms.py
@@ -0,0 +1,52 @@
+# Problems  on Loops( for ,while )
+#
+# Print Numbers: Print numbers  from 1 to 10 using a loop.
+#
+# n=int(input("enter your number"))
+# if n>=0:
+#     for i in range(1,n+1):
+#         print(f"{i} the number")
+# else:
+#     print(f"{n} give me number >=0")
+from itertools import count
+
+# print(f"{n} the number are")
+# Sum of Numbers: Calculate the sum  of numbers from  1 to n.
+
+# n=int(input("enter the your n number "))
+# if n>=0:
+#     total=0
+#     for i in range(1,n+1):
+#         total +=i
+#         print(f"{i} the total sum is ",total)
+# else:
+#     print(f"{n} give me >=0")
+#
+# Factorial: Compute the factorial of  a number.
+
+# n=int(input("enter ur number "))
+# fact=1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(fact)
+
+# import math
+# print(f"{n}* fact of this number is : ",math.factorial(n))
+#
+# Multiplication Table: Print the  multiplication  table for a given number.
+# n=int(input("enter a number is : "))
+# for i in range(1,11):
+#     print(f"{i} x {n}  =  {i*n}")
+# #
+# Count Digits: Count the number  of digits in an integer.
+#
+# from itertools import count
+#
+# n = int(input("Enter your number: "))
+#
+# for i in count(n):
+#     print(i)
+#     if i >= n + 10:
+#         break
+
+
