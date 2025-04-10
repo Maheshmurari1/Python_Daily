8aa67ad625302651651384b928df19fa22840be3
MaheshMurari <maheshmurari.data@gmail.com>
2025-04-10 11:42:44 +0530

    Add files via upload

---
+++ variables_prblms.py
@@ -0,0 +1,79 @@
+"""
+1. Problems on Variables & Basic Operations
+
+    Swap Two Variables: Write code to swap the values of two variables a and b."""
+from operator import concat
+
+# a=10
+# b=20
+# print("a values orginal  is : " ,a," :b values orginal  is : " ,b)
+# a,b=b,a
+# print("a values after  is : " ,a," :b values after  is : " ,b)
+
+
+
+# Convert Temperature: Convert a temperature from Celsius to Fahrenheit.
+
+# c=int(input("enter your temperature value : "))
+# F = (c * 9 / 5) + 32
+#
+# print(f"{c},Celsius  value to Fahrenheit ",F)
+
+
+    # Calculate Area: Compute the area of a circle given its radius.
+# r=int(input("enter your radius value is : "))
+# area=3.14*r*r
+# print(f"{r}  radius value is total of area is : ",area)
+
+
+    # String Concatenation: Combine two strings first_name and last_name into a full name.
+
+# fst_name =str(input("enter your fst_name is : "))
+# lst_name=str(input("enter lst_name is : "))
+#
+#print(concat(fst_name,lst_name))
+
+# Modulo Operation: Check if a number is even or odd using the modulo operator.
+# n=int(input("enter a number is : "))
+# if n%2==0:
+#     print(f"{n} is even number")
+# else:
+#     print(f"{n} is odd number ")
+
+
+    # User Input: Take two numbers as input and print their sum.
+# num1 =int(input("enter a your first number "))
+# num2=int(input("enter a your second number "))
+#
+# print(f"{num1} and {num2} is total value is :",{sum([num1,num2])})
+
+    # Type Conversion: Convert a string "123" to an integer and add 10 to it.
+# name=input("enter your number is : " )
+#
+# print(type(name))
+# number=int(name)
+# result =number + 10
+# print(result)
+
+
+# Calculate BMI: Compute BMI using weight (kg) and height (m).
+# bmi = weight / (height ** 2)
+
+# weight=float(input("enter the weight value is : "))
+# height=float(input("enter the height values is : "))
+#
+# bmi = weight / (height ** 2)
+#
+# print(f" weight {weight} values is and height  {height} value is ", bmi)
+
+
+    # String Repetition: Print your name 10 times using string multiplication.
+# name=str(input("enter your name is : "))
+# for i in range(10):
+#     print(f"{i+1},{name}")
+# Currency Converter: Convert USD to INR (assume 1 USD = 75 INR).
+
+# usd=int(input("enter a ur usd rupees : "))
+# inr=usd*75
+# print(f"{usd} is converted {inr}")
+
